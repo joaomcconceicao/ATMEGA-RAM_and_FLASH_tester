7b581447b94af628c2c71a9d805c6fa7ce782033
João Conceição <49520740+joaomcconceicao@users.noreply.github.com>
2020-12-23 12:20:06 +0000

    Update main.cpp

--- AVR-RAM_and_FLASH_tester/src/main.cpp
+++ AVR-RAM_and_FLASH_tester/src/main.cpp
@@ -1,23 +1,25 @@
 /*
-	ATMEGA RAM and FLASH tester example 	Rev 0.1
+	ATMEGA RAM and FLASH tester example 	Rev 1.0
 	Developed by: Francisco Oliveira & Joao Conceicao
-    10/12/2020
+   	17/12/2020
 
     CHECK THE GITHUB'S README FOR AN EXPLANATION OF HOW TO USE THIS EXAMPLE
 */
 
-#include <avr_test_ram_and_flash.h>
+FLASH_CHECKSUM_SIZE PROGRAM_CHECKSUM = 0x0000;
 
-#define TEST_RAM 1 // uncomment to test the RAM (if RAM_INJECT_FAULT is defined: 0 for r0 part other value for r1 part of MATS++)
-//#define RAM_INJECT_FAULT 0x0000  // uncomment to inject a fault (value sets the address to be corrupted)
-
-#define TEST_FLASH 1 // uncomment to test the FLASH (value doesn't matter)
-//#define FLASH_INJECT_FAULT 0x0000 // uncomment to inject a fault (value sets the address (try to use low values (on the ATMEGA328p >0x00FF) because it might corrupt the bootloader !!!))
-
-#define RESET_CHECKSUM 1
-
-int main(){
-
- 
-  
+void TestError(void){
+	
+	DDRB |= (1<<PB5);
+	PORTB |= (1<<PB5);
+	
+	while(1);
 }
+
+
+
+int main(void)
+{
+
+}
+
